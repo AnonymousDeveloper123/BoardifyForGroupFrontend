a5d967ab25157549a77007f84b8da3891df6b037
AnonymousDevelope <anonymousdeveloper2005@gmail.com>
2024-10-09 03:10:31 +0500

    add new components --- button,input field, adding router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import {ComponentList} from './pages'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path:"/component-list",
+    element:<ComponentList/>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import ForgotPassPage from "./forgot-pass.page";
 import SetNewPassPage from "./set-new-pass.page";
 import LoginPage from "./login.page";
 import ConfirmCodePage from "./confirm-code.page";
-
-export { ForgotPassPage, SetNewPassPage, LoginPage, ConfirmCodePage }
+import ComponentList from "./component-list.page";
+export { ForgotPassPage, SetNewPassPage, LoginPage, ConfirmCodePage,ComponentList }
