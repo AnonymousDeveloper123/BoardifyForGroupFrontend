610bb3a8687cb7ed81986b19588d2cf988fc742c
AnonymousDevelope <anonymousdeveloper2005@gmail.com>
2024-10-09 09:14:31 +0500

    add react icons

--- src/components/input.component.jsx
+++ src/components/input.component.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line react/prop-types
-const Input = ({ icon,leftIcon,rightIcon, ...props }) => {
+const Input = ({ Icon,leftIcon,rightIcon, ...props }) => {
     return (
       <div className="flex flex-row w-[250px] px-2 py-1 gap-2 items-center bg-white rounded-xl">
         <div className={`w-5 flex items-center justify-center text-primary cursor-pointer border-e-2 px-3 py-1 h-full ${leftIcon ? 'block' : 'hidden'}`}>
-        {icon && leftIcon && <span>{icon}</span>} 
+        {Icon && leftIcon && <span>{<Icon />}</span>} 
         </div>
         <input 
           {...props} 
@@ -11,7 +11,7 @@ const Input = ({ icon,leftIcon,rightIcon, ...props }) => {
           className="px-3 py-2 bg-transparent text-primary rounded-xl w-full outline-none border-none"
         />
         <div className={`w-5 flex items-center justify-center text-primary cursor-pointer border-s-2 px-3 py-1 h-full ${rightIcon ? 'block' : 'hidden'}`}>
-        {icon && rightIcon && <span>{icon}</span>} 
+        {Icon && rightIcon && <span>{<Icon/>}</span>} 
         </div>
       </div>
     );
--- src/pages/component-list.page.jsx
+++ src/pages/component-list.page.jsx
@@ -1,9 +1,10 @@
+import { BiSearch } from "react-icons/bi"
 import { Button,Input } from "../components"
 const ComponentList = () => {
   return (
     <div className="flex flex-row gap-10 items-center">
         <Button />
-        <Input rightIcon={true} leftIcon={true} icon={"K"}/>
+        <Input rightIcon={true} Icon={BiSearch} leftIcon={true} icon={"K"}/>
     </div>
   )
 }
