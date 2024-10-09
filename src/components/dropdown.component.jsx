2ee7e38ece80d6310082bf7900d3e02ceadcbad5
Usnatdin <anonymousdeveloper2005@gmail.com>
2024-10-09 12:48:29 +0500

    add new components to component foler

--- src/components/dropdown.component.jsx
+++ src/components/dropdown.component.jsx
@@ -1,28 +1,28 @@
 import { Dropdown } from "flowbite-react"
-const DropdownComponent = () => {
+import { useEffect, useState } from "react"
+
+// eslint-disable-next-line react/prop-types
+const DropdownComponent = ({items}) => {
+    useEffect(()=>{
+        // eslint-disable-next-line react/prop-types
+        items.map((item)=>{
+            console.log(item.name)
+        })
+    })
+    const [choose,setChoose]=useState("tanlov");
+    const onChange = (item)=>[
+        setChoose(item)
+    ]
     return (
-        <div className="dropdown">
-            <button id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-            </svg>
-            </button>
-            <div id="dropdownDelay" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDelayButton">
-                    <li>
-                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <Dropdown label={choose}>
+            {
+                // eslint-disable-next-line react/prop-types
+                items.map((item,i)=>{
+                    // eslint-disable-next-line react/jsx-key
+                    return (<Dropdown.Item key={i} onClick={()=>onChange(item.name)}>{item.name}</Dropdown.Item>)
+                })
+            }   
+        </Dropdown>
     )
 }
 
--- src/components/input.component.jsx
+++ src/components/input.component.jsx
@@ -1,16 +1,15 @@
 // eslint-disable-next-line react/prop-types
-const Input = ({ Icon,leftIcon,rightIcon, ...props }) => {
+const Input = ({ Icon,leftIcon,rightIcon,RightIconClick,LeftIconClick,...props }) => {
     return (
       <div className="flex flex-row w-[250px] px-2 py-1 gap-2 items-center bg-white rounded-xl">
-        <div className={`w-5 flex items-center justify-center text-primary cursor-pointer border-e-2 px-3 py-1 h-full ${leftIcon ? 'block' : 'hidden'}`}>
+        <div onClick={LeftIconClick} className={`w-5 flex items-center justify-center text-primary cursor-pointer border-e-2 px-4 py-1 h-full ${leftIcon ? 'block' : 'hidden'}`}>
         {Icon && leftIcon && <span>{<Icon />}</span>} 
         </div>
         <input 
-          {...props} 
-          placeholder="Input..." 
-          className="px-3 py-2 bg-transparent text-primary rounded-xl w-full outline-none border-none"
+          {...props}  
+          className="px-3 py-2 bg-transparent ring-0 ring-offset-0 text-primary rounded-xl w-full outline-none border-none"
         />
-        <div className={`w-5 flex items-center justify-center text-primary cursor-pointer border-s-2 px-3 py-1 h-full ${rightIcon ? 'block' : 'hidden'}`}>
+        <div onClick={RightIconClick} className={`w-5 flex items-center justify-center text-primary cursor-pointer border-s-2 px-4 py-1 h-full ${rightIcon ? 'block' : 'hidden'}`}>
         {Icon && rightIcon && <span>{<Icon/>}</span>} 
         </div>
       </div>
